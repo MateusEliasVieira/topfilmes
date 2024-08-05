20ff117d7d177513474dc2c1212bf02cd6ecdb3a
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-08-05 17:24:36 -0300

    feito avaliação de filme

--- src/main/java/com/ifgoiano/topfilmes/domain/service/impl/AvaliationServiceImpl.java
+++ src/main/java/com/ifgoiano/topfilmes/domain/service/impl/AvaliationServiceImpl.java
@@ -2,10 +2,13 @@ package com.ifgoiano.topfilmes.domain.service.impl;
 
 import com.ifgoiano.topfilmes.domain.domainException.BusinessRulesException;
 import com.ifgoiano.topfilmes.domain.model.Avaliation;
+import com.ifgoiano.topfilmes.domain.model.Movie;
 import com.ifgoiano.topfilmes.domain.model.User;
 import com.ifgoiano.topfilmes.domain.repository.AvaliationRepository;
 import com.ifgoiano.topfilmes.domain.repository.UserRepository;
 import com.ifgoiano.topfilmes.domain.service.AvaliationService;
+import com.ifgoiano.topfilmes.domain.service.MovieService;
+import com.ifgoiano.topfilmes.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,24 +21,59 @@ public class AvaliationServiceImpl implements AvaliationService {
     @Autowired
     private AvaliationRepository repository;
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
+    @Autowired
+    private MovieService movieService;
 
     @Transactional(readOnly = false)
     @Override
     public Avaliation add(Avaliation avaliation) {
-        if (repository.findByUsuarioId(avaliation.getUser().getIdUser()).isEmpty()) {
-            Avaliation avaliationSaved = repository.save(avaliation);
-            User user = userRepository.findById(avaliationSaved.getUser().getIdUser()).orElseThrow(() -> new BusinessRulesException("Não foi possível vincular o usuário para a avaliação, pois o id do usuário é inválido!"));
-            avaliationSaved.setUser(user);
-            return avaliationSaved;
-        } else {
-            throw new BusinessRulesException("O usuário de id " + avaliation.getUser().getIdUser() + " já realizou uma avaliação sobre o filme de id " + avaliation.getMovie().getIdMovie());
-        }
+
+
+        if (avaliation.getUser().getIdUser() == null)
+            throw new BusinessRulesException("Informe o usuário!");
+
+        User user = userService.searchById(avaliation.getUser().getIdUser()); // se passar, existe!
+
+
+        if(avaliation.getMovie().getIdMovie() == null)
+            throw new BusinessRulesException("Selecione o filme!");
+
+        Movie movie = movieService.searchById(avaliation.getMovie().getIdMovie()); // se passar, existe!
+
+
+        if(avaliation.getScore() > 5 || avaliation.getScore() < 0)
+            throw new BusinessRulesException("A pontuação deve ser entre 0 a 5 estrelas!");
+
+
+        // busca avaliação pelo fk id_usuario da tabela avaliação
+        if(repository.findByUsuarioId(user.getIdUser()).isPresent())
+            throw new BusinessRulesException("O usuário "+user.getName()+" já fez uma avaliação do filme "+movie.getTitle()+"!");
+
+        avaliation.setMovie(movie);
+        avaliation.setUser(user);
+
+        Avaliation avaliation_saved = repository.save(avaliation);
+
+        user.getAvaliations().add(avaliation_saved);
+        movie.getAvaliations().add(avaliation_saved);
+
+        userService.update(user);
+        movieService.update(movie);
+
+        return avaliation_saved;
+
     }
 
     @Transactional(readOnly = false)
     @Override
     public Avaliation update(Avaliation avaliation) {
+
+        if(avaliation.getIdAvaliation() == null)
+            throw new BusinessRulesException("Informe a avaliação!");
+
+        searchById(avaliation.getIdAvaliation()); // se passar, existe
+
         return repository.save(avaliation);
     }
 
@@ -55,7 +93,14 @@ public class AvaliationServiceImpl implements AvaliationService {
     @Override
     public void deleteById(Long idAvaliation) {
         try {
-            searchById(idAvaliation);
+
+            Avaliation avaliation = searchById(idAvaliation);
+
+            avaliation.setUser(null);
+            avaliation.setMovie(null);
+
+            update(avaliation);
+
             repository.deleteById(idAvaliation);
         } catch (BusinessRulesException businessRulesException) {
             throw new BusinessRulesException("Não existe avaliação com id " + idAvaliation + " para ser deletada!");
